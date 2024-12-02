b01af09bcdd481532606c325e3ff19b7cd00d657
GeoffreyPaul3 <geofreypaul40@gmail.com>
2024-12-02 10:11:49 +0200

    Payment fixing

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -1,11 +1,16 @@
+import { Metadata } from 'next';
 import PaymentConfirmation from './components/PaymentComfirmation';
 
 interface Props {
   searchParams?: { tx_ref: string };
 }
 
+export const metadata: Metadata = {
+  title: 'Payment Confirmation',
+};
+
 const Page = ({ searchParams }: Props) => {
-  if (!searchParams?.tx_ref) {
+  if (!searchParams || !searchParams.tx_ref) {
     throw new Error('Transaction reference is missing.');
   }
 
